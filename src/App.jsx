f755156e640dc88a9b82653871c79eaf5f933ac7
alireza <bayatalireza86@gmail.com>
2025-03-08 00:02:58 +0330

    `Refactor navigation, layout, and styling across multiple components and pages`

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import Cities from "./components/CityList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} >
+          <Route index element={<Cities />} />
+          <Route path="cities" element={<p>cities list</p>} />
+          <Route path="countries" element={<p>country list</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <h1>Page Not Found...</h1>
-      <PageNav />
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
