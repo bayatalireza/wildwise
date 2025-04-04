3670f0dac5d324276b8d7555ce76468abd209cd0
alireza <bayatalireza86@gmail.com>
2025-04-04 21:21:57 +0330

    `Added AuthProvider and useAuth hook, updated components to use authentication context`

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Login.module.css";
 import Button from "../components/Button";
+import { useAuth, isAuthenticated } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if ((email, password)) {
+      login(email, password);
+    }
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated()) {
+        navigate("/app", {replace: true});
+      }
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
